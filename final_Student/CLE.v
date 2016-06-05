aba60981a2006417edf2d878a8526d767a95b4ea
insectwang <frankwang50302@gmail.com>
2016-06-05 22:21:14 +0800

    寫一些架構

--- final_Student/CLE.v
+++ final_Student/CLE.v
@@ -12,16 +12,41 @@ output        finish;
 
 //parameter declaration
 parameter IDLE      =2'd0;
-parameter PROCESS   =2'd1;
-parameter DONE      =2'd2;
+parameter READ      =2'd1;
+parameter PROCCESS  =2'd2;
+parameter DONE      =2'd3;
 
 //reg declaration
 
 reg [6:0]   rom_a,    n_rom_a;
 reg [9:0]   sram_a,   n_sram_a;
 reg [7:0]   sram_d,   n_sram_d;
+reg [1:0]   state,    next_state;
 reg         sram_wen, n_sram_wen;
 reg         finish,   n_finish;
 
+always@(posedge clk) begin
+    if(reset) begin
+        state <= IDLE;
+    end
+    else begin
+        state <= next_state;
+    end
+end
+
+always@(*) begin
+    case(state)
+        IDLE: begin
+        end
+
+        READ: begin
+        end
+        
+        PROCCESS: begin
+        end
+
+        DONE: begin
+        end
+end
 
 endmodule
